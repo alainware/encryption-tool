15bcbc1cc6ced0106a0e966ee5ed4b4b5f32bab4
alainware <irving6258@gmail.com>
2022-10-26 00:04:51 -0500

    Update: Not yet message

---
+++ docs/js/main.js
@@ -0,0 +1,5 @@
+function notYet() {
+    alert("The application is not ready for download!\nSorry buddy :(");
+    document.getElementById("download-button").disabled = true;
+    document.getElementById("download-button").hidden = true;
+}
